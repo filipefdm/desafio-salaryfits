3afa9478888d8ebaf4c0e02a447ee69b690bbd5c
Filipe <femotta96@gmail.com>
2023-05-25 15:40:58 -0300

    feat(weather): implement route to fetch 5-day weather forecast

---
+++ src/routes/forecastRoutes.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import * as weatherService from "../services/weatherService";
+
+const router = express.Router();
+
+// OBTER A PREVISÃO DO TEMPO PARA 5 DIAS:
+router.get("/5days", async (req: Request, res: Response) => {
+  try {
+    const city = req.query.city as string;
+    const forecast = await weatherService.getFiveDayWeatherForecast(city);
+    res.json(forecast);
+  } catch (error) {
+    console.error("Erro ao obter a previsão do tempo para 5 dias:", error);
+    res
+      .status(500)
+      .json({ error: "Erro ao obter a previsão do tempo para 5 dias." });
+  }
+});
+
+export default router;
